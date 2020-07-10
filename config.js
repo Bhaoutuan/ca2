bed479079755bb3901c19b66f643d13129b05dce
Bhaoutuan <66213692+Bhaoutuan@users.noreply.github.com>
2020-07-10 22:40:40 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "568547592548384800", //kendi IDınızı yazınız
+    "admins": ["568547592548384800"],
+    "support": ["568547592548384800"],
+    "token": "NzMxMTY1NDczMjM1OTI3MDcy.XwjAVA.zW0_DMKZBR50t7lz_5GL7LHFTKw", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "AioDmS5314KE4LCJ9Sl834DMPe6806OT", //botunuzun secretini yazınız
+      "callbackURL": `https://lenux.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "https://lenux.glitch.me/", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
